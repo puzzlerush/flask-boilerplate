a81be237cd551e8a07164cebf7d75dce4472fc26
tacticaltofu <dingkevin@gmail.com>
2021-11-28 22:54:04 -0500

    use gunicorn for prod

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 flask db upgrade
-flask run --host=0.0.0.0
+
+if [ "$FLASK_ENV" == "development" ]; then
+    flask run --host=0.0.0.0
+else
+    gunicorn --bind 0.0.0.0:5000 app:app
+fi
